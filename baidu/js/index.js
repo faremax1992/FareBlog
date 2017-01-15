148b494377d25f3d1488f4e607d4ba2b5c608ae0
faremax <faremax@aliyun.com>
2017-01-15 22:44:05 +0800

    add ZOL & revise aliyun

--- baidu/js/index.js
+++ baidu/js/index.js
@@ -1,123 +1,123 @@
 (function(){
 
-	//以下绑定了一些事件
-	var firstMask = document.getElementById('first_mask');
-	var secondMask = document.getElementById('second_mask');
-	var thirdMask = document.getElementById('third_mask');
-	var mask = document.getElementById("mask");
+  //以下绑定了一些事件
+  var firstMask = document.getElementById('first_mask');
+  var secondMask = document.getElementById('second_mask');
+  var thirdMask = document.getElementById('third_mask');
+  var mask = document.getElementById("mask");
 
-	function off(){
-		mask.style.display = "none";
-		secondMask.style.display = "none";
-		secondMask.style.display = "none";
-		firstMask.style.display = "block";
-	}
-	var confirm = document.getElementById("mask_confirm");
-	var confirm2 = document.getElementById("mask_confirm2");
-	var close = document.getElementById("mask_close");
-	confirm.onclick = off;
-	confirm2.onclick = off;
-	close.onclick = off;
+  function off(){
+    mask.style.display = "none";
+    secondMask.style.display = "none";
+    thirdMask.style.display = "none";
+    firstMask.style.display = "block";
+  }
+  var confirm = document.getElementById("mask_confirm");
+  var confirm2 = document.getElementById("mask_confirm2");
+  var close = document.getElementById("mask_close");
+  confirm.onclick = off;
+  confirm2.onclick = off;
+  close.onclick = off;
 
-	function log(){
-		mask.style.display = "block";
-	}
+  function log(){
+    mask.style.display = "block";
+  }
 
-	var login = document.getElementById("login");
-	var login2 = document.getElementById("login2");
-	var history = document.getElementById("history");
-	history.onclick = log;
-	login.onclick = log;
-	login2.onclick = log;
+  var login = document.getElementById("login");
+  var login2 = document.getElementById("login2");
+  var history = document.getElementById("history");
+  history.onclick = log;
+  login.onclick = log;
+  login2.onclick = log;
 
-	var goto_qr = document.getElementById('gotoqr');
-	var goto_qr2 = document.getElementById('gotoqr2');
-	var goto_pc = document.getElementById('gotopc');
+  var goto_qr = document.getElementById('gotoqr');
+  var goto_qr2 = document.getElementById('gotoqr2');
+  var goto_pc = document.getElementById('gotopc');
 
-	goto_qr.onclick = function(){
-		firstMask.style.display = "none";
-		secondMask.style.display = "block";
-	};
-	goto_qr2.onclick = function(){
-		thirdMask.style.display = "none";
-		secondMask.style.display = "block";
-	};
-	goto_pc.onclick = function(){
-		firstMask.style.display = "block";
-		secondMask.style.display = "none";
-	};
+  goto_qr.onclick = function(){
+    firstMask.style.display = "none";
+    secondMask.style.display = "block";
+  };
+  goto_qr2.onclick = function(){
+    thirdMask.style.display = "none";
+    secondMask.style.display = "block";
+  };
+  goto_pc.onclick = function(){
+    firstMask.style.display = "block";
+    secondMask.style.display = "none";
+  };
 
-	var gotoPhone = document.getElementById('gotophone');
-	var gotoUser = document.getElementById('gotouser');
+  var gotoPhone = document.getElementById('gotophone');
+  var gotoUser = document.getElementById('gotouser');
 
-	gotoPhone.onclick = function(){
-		firstMask.style.display = "none";
-		thirdMask.style.display = "block";
-	};
-	gotoUser.onclick = function(){
-		thirdMask.style.display = "none";
-		firstMask.style.display = "block";
-	};
+  gotoPhone.onclick = function(){
+    firstMask.style.display = "none";
+    thirdMask.style.display = "block";
+  };
+  gotoUser.onclick = function(){
+    thirdMask.style.display = "none";
+    firstMask.style.display = "block";
+  };
 
-	var downChoice = document.getElementById('down').getElementsByTagName('li');
-	var downContent = document.getElementsByClassName('down_content');
-	for(var i = 0; i < downChoice.length; i++){
-		downChoice[i].onclick = function(){
-			for(var j = 0; j < downChoice.length; j++){
-				downChoice[j].className = "";
-				downContent[j].style.display ="none";
-			}
-			this.className = "checked";
-			var index = parseInt(this.getAttribute('data-num'));
-			downContent[index].style.display = "block";
-		};
-	}
+  var downChoice = document.getElementById('down').getElementsByTagName('li');
+  var downContent = document.getElementsByClassName('down_content');
+  for(var i = 0; i < downChoice.length; i++){
+    downChoice[i].onclick = function(){
+      for(var j = 0; j < downChoice.length; j++){
+        downChoice[j].className = "";
+        downContent[j].style.display ="none";
+      }
+      this.className = "checked";
+      var index = parseInt(this.getAttribute('data-num'));
+      downContent[index].style.display = "block";
+    };
+  }
 
-	var downClose = document.getElementById("down_close");
-	var down = document.getElementById('down');
-	downClose.onclick = function(){
-		down.style.display = "none";
-	};
+  var downClose = document.getElementById("down_close");
+  var down = document.getElementById('down');
+  downClose.onclick = function(){
+    down.style.display = "none";
+  };
 
-	var sConf = document.getElementById('s_conf');
-	var sAdv = document.getElementById('s_adv');
+  var sConf = document.getElementById('s_conf');
+  var sAdv = document.getElementById('s_adv');
 
-	sConf.onclick = function(){
-		down.style.display = "block";
-		downChoice[0].click();
-	};
-	sAdv.onclick = function(){
-		down.style.display = "block";
-		downChoice[1].click();
-	};
+  sConf.onclick = function(){
+    down.style.display = "block";
+    downChoice[0].click();
+  };
+  sAdv.onclick = function(){
+    down.style.display = "block";
+    downChoice[1].click();
+  };
 
-	var pos = document.getElementById('pos');
-	var mask = document.getElementById('mask');
-	
-	stopPro(mask, "click"); //防止登录遮罩层内事件冒泡
-	stopPro(down, "click"); //防止下拉菜单层内事件冒泡
-	stopPro(pos, "click"); //防止设置菜单层内事件冒泡
+  var pos = document.getElementById('pos');
+  var mask = document.getElementById('mask');
 
-	//利用事件冒泡实现以下一个功能
-	//点击设置浮动框（在上方）之外部分将该框隐藏
-	
-	//自定义函数，阻止事件冒泡
-	function stopPro(target, event){
-		if(target.addEventListener){
-			target.addEventListener(event, function(e){
-				e = e || window.event;
-				if(e.stopPropagation) e.stopPropagation();
-				else e.cancelBubble = true;
-			},false);
-		} else {
-			target.attachEvent("on" + event,function(e){
-				e = e || window.event;
-				if(e.stopPropagation) e.stopPropagation();
-				else e.cancelBubble = true;
-			});
-		}
-	}
-	document.addEventListener("click", function(){
-		down.style.display="none";
-	}, false);
+  stopPro(mask, "click"); //防止登录遮罩层内事件冒泡
+  stopPro(down, "click"); //防止下拉菜单层内事件冒泡
+  stopPro(pos, "click"); //防止设置菜单层内事件冒泡
+
+  //利用事件冒泡实现以下一个功能
+  //点击设置浮动框（在上方）之外部分将该框隐藏
+
+  //自定义函数，阻止事件冒泡
+  function stopPro(target, event){
+    if(target.addEventListener){
+      target.addEventListener(event, function(e){
+        e = e || window.event;
+        if(e.stopPropagation) e.stopPropagation();
+        else e.cancelBubble = true;
+      },false);
+    } else {
+      target.attachEvent("on" + event,function(e){
+        e = e || window.event;
+        if(e.stopPropagation) e.stopPropagation();
+        else e.cancelBubble = true;
+      });
+    }
+  }
+  document.addEventListener("click", function(){
+    down.style.display="none";
+  }, false);
 }());
--- tmall/js/fixedbar.js
+++ tmall/js/fixedbar.js
@@ -1,14 +1,25 @@
 (function(){
-	var $verSpan = $("#ver_bar>span");
-	var $vDetail = $(".v_detail");
-	$verSpan.hover(function(){
-		var num = $verSpan.index(this);
-		var offset = num >= 5 ? 2 : 1;
-		if(num == 11) $vDetail.eq(9).show().animate({"right": "31px"}, {duration: "fast"});
-		else if(num != 0 && num != 4) $vDetail.eq(num - offset).show().animate({"right": "35px"}, {duration: "fast"});
+  //right bar
+  var $verSpan = $("#ver_bar>span");
+  var $vDetail = $(".v_detail");
+  $verSpan.hover(function(){
+    var num = $verSpan.index(this);
+    var offset = num >= 5 ? 2 : 1;
+    if(num == 11) $vDetail.eq(9).show().animate({"right": "31px"}, {duration: "fast"});
+    else if(num != 0 && num != 4) $vDetail.eq(num - offset).show().animate({"right": "35px"}, {duration: "fast"});
 
-	},
-	function(){
-		$vDetail.animate({"right": "55px"}, {duration: 0}).hide();
-	});
+  },
+  function(){
+    $vDetail.animate({"right": "55px"}, {duration: 0}).hide();
+  });
+
+  //top bar
+  console.log($("body"));
+  $("body")[0].onscroll = function(){
+    console.log($("body")[0].scrollTop > window.screen.height)
+    if($("body")[0].scrollTop > window.screen.height)
+      $("#top_search").animate({"top": "0px"}, {duration: "fast", queue: false});
+    else
+      $("#top_search").animate({"top": "-50px"}, {duration: "fast", queue: false});
+  }
 }());
