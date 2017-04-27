8797a98c74700d7148c75ff2eeab5358f13188d5
faremax <faremax@aliyun.com>
2017-04-27 14:28:46 +0800

    fix bugs

--- websites/tetris/js/config.js
+++ websites/tetris/js/config.js
@@ -1,12 +1,12 @@
 (function(window){
-	"use strict";
-	window.TetrisConfig = {
-		rows: 20,
-		cols: 13	,
-		speed: 1000,
-		constSpeed: 1000,
-		muted: false,
-		grid: false,
-		timer: null
-	};
+  "use strict";
+  window.TetrisConfig = {
+    rows: 20,
+    cols: 13,
+    speed: 1000,
+    constSpeed: 1000,
+    muted: false,
+    grid: false,
+    timer: null
+  };
 })(window);
